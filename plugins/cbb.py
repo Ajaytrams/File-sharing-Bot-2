862a8c7d5810eb414228c4f706f33c7556a69a39
Ajaytrams <110770742+Ajaytrams@users.noreply.github.com>
2023-01-19 08:11:07 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={@Ajaytrams}'>Ajay Trams</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Ajaytrams/File-sharing-Bot-2'>Click here</a>\n○ Main Channel : @Aj_creations_ajay1\n○ Support Group : @Aj_creations_ajay</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={https://t.me/ajaytrams}'>Ajay Trams</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Ajaytrams/File-sharing-Bot-2'>Click here</a>\n○ Main Channel : @Aj_creations_ajay1\n○ Support Group : @Aj_creations_ajay</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
